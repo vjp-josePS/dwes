9c9f850918effaa2eda90cd1cd0f3046d4e6107b
1DAW-Jose Padilla <jpadillas03@informatica.iesvalledeljerteplasencia.es>
2024-10-08 18:53:59 +0200

    fin doc y inicio doc

--- views/about.view.php
+++ views/about.view.php
@@ -1,26 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-	  <title>PhotographItem-Responsive Theme | About us</title>
-
-  	<!-- Bootstrap core css -->
-  	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
-  	<!-- Bootstrap core css -->
-  	<link rel="stylesheet" type="text/css" href="css/style.css">
-  	<!-- Font Awesome icons -->
-  	<link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-</head>
-<body id="page-top">
+<?php include __DIR__.'/partials/inicio-doc.part.php'?>
 
 <!-- Navigation Bar -->
    <nav class="navbar navbar-fixed-top navbar-default">
@@ -212,6 +190,8 @@
          <p>Photography Fanatic Template &copy; 2017</p>
      </div>
    </footer>
+
+   <?php include __DIR__.'/partials/fin-doc.part.php'?>
    
 <!-- Jquery -->
    <script type="text/javascript" src="js/jquery.min.js"></script>
